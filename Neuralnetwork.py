1a0f1d51ee590543c37adfda2fda1ead21f5191a
Obakeng-Mogale <mogaleobakeng68@gmail.com>
2025-07-07 19:58:13 +0200

    fixed forward and backward pass

--- Neuralnetwork.py
+++ Neuralnetwork.py
@@ -5,6 +5,7 @@ class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1,n_neurons))
+
     def forward(self, X):
         self.inputs = X
         self.output = np.dot(X,self.weights)+self.biases
@@ -38,11 +39,11 @@ class loss_mse(Loss):
     """
     def forward(self, y_pred, y_true):
         self.diff = (y_pred - y_true)
-        self.numerator = self.diff**2
-        return self.numerator
-    def backward(self):
-        self.dinputs = 2*self.diff/np.size(self.numerator)
-        return self.inputs
+        self.output= self.diff**2
+        return self.output
+    def backward(self,dvalues=0, y_true=0):
+        self.dinputs = 2*self.diff/np.size(self.output)
+        return self.dinputs
     
 class loss_CategoricalCrossEntropy(Loss):
     """
@@ -55,9 +56,9 @@ class loss_CategoricalCrossEntropy(Loss):
 
         elif len(y_true.shape) ==2:
             correct_conf = np.sum(y_clipped*y_true, axis = 1)
-
-        negative_likelihoods = -np.log(correct_conf)
-        return negative_likelihoods
+        #negative likelyhood
+        self.output = -np.log(correct_conf)
+        return self.output
   
     def backward(self, dvalues, y_true):
         # Number of samples
@@ -81,6 +82,7 @@ class Activation_ReLU:
     def forward(self,inputs):
         self.inputs = inputs #remember input values
         self.output = np.maximum(0,inputs)
+        return self.output
     def backward(self,dvalues):
         # Since we need to modify the original variable,
         #  # let's make a copy of the values first
@@ -131,7 +133,7 @@ class optimizer_SGD:
 
     momentum to improve will be w(new) = w(previous)-alpha*dl/dw + momentum factor*prevweight updates
     """
-    def __init__(self, learning_rate = 1,decay = 0.0, momentum = 0.0):
+    def __init__(self, learning_rate = 1,decay = 0.0, momentum = 0.9):
         self.learning_rate = learning_rate
         self.current_learning_rate = learning_rate
         self.decay = decay
@@ -157,6 +159,7 @@ class optimizer_SGD:
 
             bias_updates = self.momentum * layer.bias_momentums - self.current_learning_rate * layer.dbiases
             
+            layer.bias_updates = bias_updates
         else:
             # Standard Gradient Descent update rule: parameter = parameter - learning_rate * gradient
             weight_updates = - self.current_learning_rate * layer.dweights
@@ -167,7 +170,18 @@ class optimizer_SGD:
 
     def post_update(self):
         self.iterations+=1
+
+
+
 class Sequential:
+    optimizers = {
+        'sgd': optimizer_SGD,
+        
+    }
+    loss_func = {
+        'mse': loss_mse,
+        'cce': loss_CategoricalCrossEntropy
+    }
     def __init__(self, layers:list):
         self.weights = None
         self.biases = None
@@ -175,23 +189,38 @@ class Sequential:
 
     
     def compile_net(self, optimizer,loss, metrics:list = None, learning_rate = 1):
-        self.loss = loss()
-        self.optimizer = optimizer(learning_rate)
+        optimizers = {
+        'sgd': optimizer_SGD,
+        
+        }
+        loss_func = {
+            'mse': loss_mse,
+            'cce': loss_CategoricalCrossEntropy
+        }
+        self.loss = loss_func[loss]()
+       
+        self.optimizer = optimizers[optimizer](learning_rate)
         self.metrics = metrics
         return 
     
-    def fit(self,input_data, output_data, epochs = 5, batch_size = 1, shuffle_batch = False):
+    def fit(self,input_data, output_data, epochs = 5, batch_size = -1, shuffle_batch = False):
         """
         function takes in input data and output data aswell as an optimizer and metric(optional)
 
         """
-        x_batches,y_batches = self._create_batches(input_data,output_data,)
+    
+        x_batches,y_batches = self._create_batches(input_data,output_data,batch_size=batch_size)
         for epoch in range(epochs):
+            self.loss_avg = 0
             for X,y_true in zip(x_batches,y_batches):
                 y_pred = self._forward(X)
-                self.loss.calculate(y_pred,y_true)
+                batch_loss = self.loss.calculate(y_pred,y_true)
+                self.loss_avg+=batch_loss
+                # print('loss', l, 'acc:', accuracy)
+                
                 self._backward(y_true)
-
+                
+            print("epoch:" ,epoch, "\nloss:", self.loss_avg/np.size(x_batches))
         return 
     
     def evaluate(self, input_data, output_data):
@@ -203,19 +232,35 @@ class Sequential:
         self.output = self._forward(input_data)
         return self.output
     
+    def get_loss(self, input_data, output_data):
+        if hasattr(self, 'output'):
+            return self.loss.calculate(self.output,output_data)
+        else:
+            return 'cannot get loss: \n please try running predict or fit the model'
+        
+
     def _forward(self, input_data):
         outdata = None
+        i = 0
         for layer in self.layers:
+
             outdata = layer.forward(input_data)
             input_data = outdata
+            i+=1
+    
         return outdata
     
     def  _backward(self,ybatch):
         self.loss.backward(self.loss.output,ybatch)
+        self.optimizer.pre_update()
         prev_layer = self.loss
         for layer in reversed(self.layers):
             layer.backward(prev_layer.dinputs)
             prev_layer = layer
+            if hasattr(layer,'weights'):
+                self.optimizer.update(layer)
+        self.optimizer.post_update()
+   
         return 
 
             
@@ -236,19 +281,28 @@ class Sequential:
         """
         num_samples = input_data.shape[0]
 
-        # Optional: Shuffle the data
+        if num_samples != output_data.shape[0]:
+            raise ValueError("input_data and output_data must have the same number of samples.")
+
         if shuffle:
             indices = np.arange(num_samples)
             np.random.shuffle(indices)
             input_data = input_data[indices]
-            input_data = output_data[indices]
+            output_data = output_data[indices] # Corrected this line
 
-        # Iterate through the data in chunks of batch_size
+        if batch_size == -1:
+            batch_size = num_samples
+
+        x_batches = []
+        y_batches = []
+
         for i in range(0, num_samples, batch_size):
-            # Slice the data to get the current batch
-            x_batch,y_batch = input_data[i:i + batch_size],output_data[i:i+batch_size]
-            yield x_batch,y_batch
+            x_batch = input_data[i:i + batch_size]
+            y_batch = output_data[i:i + batch_size]
+            x_batches.append(x_batch)
+            y_batches.append(y_batch)
 
+        return x_batches, y_batches
 
 
 if __name__ == "__main__":
